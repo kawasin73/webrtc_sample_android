9e2feeb432a9dfefd2b235f0b7c8b3ccb65aa059
Kawamura Shintaro <kawasin73@gmail.com>
2017-07-02 13:45:55 +0900

    Fetch SkyWay peerId

--- app/src/main/java/com/kawasin73/webrtcsample/MainActivity.java
+++ app/src/main/java/com/kawasin73/webrtcsample/MainActivity.java
@@ -1,13 +1,128 @@
 package com.kawasin73.webrtcsample;
 
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.skyway.Peer.OnCallback;
+import io.skyway.Peer.Peer;
+import io.skyway.Peer.PeerOption;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+    private Peer peer;
+    private List<String> idList = new ArrayList<String>();
+    private MyAdapter adapter;
+    private TextView idTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        adapter = new MyAdapter(this, 0, idList);
+        listView.setAdapter(adapter);
+
+        PeerOption options = new PeerOption();
+        options.key = BuildConfig.SKYWAY_API_KEY;
+        options.domain = BuildConfig.SKYWAY_HOST;
+        peer = new Peer(this, options);
+
+        idTextView = (TextView) findViewById(R.id.id_text_view);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                refreshAllPeers();
+            }
+        });
+
+        peer.on(Peer.PeerEventEnum.OPEN, new OnCallback() {
+            @Override
+            public void onCallback(Object o) {
+                if (o instanceof String) {
+                    final String id = (String) o;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            idTextView.setText("ID: " + id);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void refreshAllPeers() {
+        peer.listAllPeers(new OnCallback() {
+            @Override
+            public void onCallback(Object o) {
+                if (o instanceof JSONArray) {
+                    JSONArray array = (JSONArray) o;
+                    idList.clear();
+                    for (int i = 0; i < array.length(); i++) {
+                        try {
+                            idList.add(array.getString(i));
+                            Log.d(TAG, "Fetched PeerId: " + array.getString(i));
+                        } catch (JSONException e) {
+                            Log.e(TAG, "Parse ListAllPeer", e);
+                        }
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (peer != null) {
+            peer.destroy();
+        }
+    }
+
+    private class MyAdapter extends ArrayAdapter<String> {
+        private LayoutInflater inflater;
+
+        MyAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<String> objects) {
+            super(context, resource, objects);
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View view = inflater.inflate(R.layout.list_item, null, false);
+            TextView textView = (TextView) view.findViewById(R.id.list_item_text);
+            String name = getItem(position);
+            textView.setText(name);
+            return view;
+        }
     }
 }
