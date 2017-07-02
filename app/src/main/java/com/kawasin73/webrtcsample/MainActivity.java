46956ca79f5215c222b36419d1482cc4f7949f17
Kawamura Shintaro <kawasin73@gmail.com>
2017-07-02 16:17:29 +0900

    Enable Call on WebRTC

--- app/src/main/java/com/kawasin73/webrtcsample/MainActivity.java
+++ app/src/main/java/com/kawasin73/webrtcsample/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,17 +20,26 @@ import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+import io.skyway.Peer.Browser.MediaConstraints;
+import io.skyway.Peer.Browser.MediaStream;
+import io.skyway.Peer.Browser.Navigator;
+import io.skyway.Peer.CallOption;
+import io.skyway.Peer.MediaConnection;
 import io.skyway.Peer.OnCallback;
 import io.skyway.Peer.Peer;
 import io.skyway.Peer.PeerOption;
 
 public class MainActivity extends AppCompatActivity {
     private static String TAG = "MainActivity";
-    private Peer peer;
     private List<String> idList = new ArrayList<String>();
     private MyAdapter adapter;
     private TextView idTextView;
+    private Peer peer;
+    private MediaConnection connection;
+    private String currentPeerId = "";
+    private String selectedPeerId = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,14 +54,23 @@ public class MainActivity extends AppCompatActivity {
         options.key = BuildConfig.SKYWAY_API_KEY;
         options.domain = BuildConfig.SKYWAY_HOST;
         peer = new Peer(this, options);
+        Navigator.initialize(peer);
 
         idTextView = (TextView) findViewById(R.id.id_text_view);
 
-        Button btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button refreshBtn = (Button) findViewById(R.id.refresh_btn);
+        refreshBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 refreshAllPeers();
+            }
+        });
+
+        Button callBtn = (Button) findViewById(R.id.call_btn);
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                call();
             }
         });
 
@@ -62,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
             public void onCallback(Object o) {
                 if (o instanceof String) {
                     final String id = (String) o;
+                    currentPeerId = id;
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -71,9 +89,25 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        peer.on(Peer.PeerEventEnum.CALL, new OnCallback() {
+            @Override
+            public void onCallback(Object o) {
+                Log.d(TAG, "CALL Event is Received");
+                Log.d(TAG, String.valueOf(o.getClass()));
+                Log.d(TAG, String.valueOf(o));
+                if (o instanceof MediaConnection) {
+                    MediaConnection connection = (MediaConnection) o;
+                    MediaStream stream = MainActivity.this.getMediaStream();
+                    connection.answer(stream);
+                    MainActivity.this.connection = connection;
+                }
+            }
+        });
     }
 
     private void refreshAllPeers() {
+        Log.d(TAG, "Refreshing");
         peer.listAllPeers(new OnCallback() {
             @Override
             public void onCallback(Object o) {
@@ -82,8 +116,12 @@ public class MainActivity extends AppCompatActivity {
                     idList.clear();
                     for (int i = 0; i < array.length(); i++) {
                         try {
-                            idList.add(array.getString(i));
-                            Log.d(TAG, "Fetched PeerId: " + array.getString(i));
+                            String id = array.getString(i);
+                            idList.add(id);
+                            Log.d(TAG, "Fetched PeerId: " + id);
+                            if (!Objects.equals(id, currentPeerId)) {
+                                selectedPeerId = id;
+                            }
                         } catch (JSONException e) {
                             Log.e(TAG, "Parse ListAllPeer", e);
                         }
@@ -99,11 +137,56 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void call() {
+        Log.d(TAG, "Calling");
+        if (peer == null) {
+            Log.i(TAG, "Call but peer is null");
+            return;
+        }
+
+        Log.d(TAG, String.format("peer.isDestroyed: %b", peer.isDestroyed));
+        Log.d(TAG, String.format("peer.isDisconnected: %b", peer.isDisconnected));
+        Log.d(TAG, "selectedPeerId: " + selectedPeerId);
+
+        if (!peer.isDestroyed && !peer.isDisconnected) {
+            MediaStream stream = getMediaStream();
+            CallOption option = new CallOption();
+            option.metadata = "test"; // TODO: metadata
+            final MediaConnection connection =  peer.call(selectedPeerId, stream, option);
+            if (connection == null) {
+                Log.d(TAG, "MediaConnection is null");
+                return;
+            }
+
+            connection.on(MediaConnection.MediaEventEnum.CLOSE, new OnCallback() {
+                @Override
+                public void onCallback(Object o) {
+                    // TODO:
+                    connection.close();
+                    MainActivity.this.connection = null;
+                }
+            });
+
+            this.connection = connection;
+        }
+    }
+
+    private MediaStream getMediaStream() {
+        MediaConstraints constraints = new MediaConstraints();
+        constraints.videoFlag = false;
+        constraints.audioFlag = true;
+        return Navigator.getUserMedia(constraints);
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         if (peer != null) {
             peer.destroy();
+        }
+        if (connection != null) {
+            connection.close();
+            this.connection = null;
         }
     }
 
